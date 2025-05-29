0e6b1e60c12991bec4057a7093f3de65346d2118
root <root@LAPTOP-J8MVT9K6.localdomain>
2025-05-29 19:14:54 +0530

    added dist

--- Backend/index.js
+++ Backend/index.js
@@ -37,6 +37,7 @@ const express = require('express')
 const path = require('path');
 const app = express()
 const cors = require('cors')
+app.use(express.static('dist'))
 app.use(cors())
 app.use(express.json())
 let notes = [
